6aaac79871f27ceac1f3ed79468748812b124d8d
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-27 10:11:49 -0300

    Create ex008.py

---
+++ exFunc/ex008.py
@@ -0,0 +1,6 @@
+#Faça uma função que informe a quantidade de dígitos de um determinado número inteiro informado.
+def contarDigitos(num:str):
+    leng = len(num)
+    return leng
+num1 = input('Digite o número: ')
+print(contarDigitos(num1))
